dc3d25c0411f6b1c154b0ebf333b3640cd8eb4a8
John Maca <jjm22778@gmail.com>
2024-11-23 16:43:56 -0500

    TODO 7 Completed.

--- app/src/main/java/edu/farmingdale/draganddropanim_demo/DragAndDropBoxes.kt
+++ app/src/main/java/edu/farmingdale/draganddropanim_demo/DragAndDropBoxes.kt
@@ -12,6 +12,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -25,10 +26,16 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -37,21 +44,14 @@ import androidx.compose.ui.draganddrop.DragAndDropEvent
 import androidx.compose.ui.draganddrop.DragAndDropTarget
 import androidx.compose.ui.draganddrop.DragAndDropTransferData
 import androidx.compose.ui.draganddrop.mimeTypes
-import androidx.compose.foundation.Canvas
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material3.Icon
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.coroutineScope
 
 
 @Composable
@@ -124,12 +124,42 @@ fun DragAndDropBoxes(modifier: Modifier = Modifier) {
             }
         }
 
-        val rotateAnimation = remember {
-            Animatable(0f)
-        }
+        val rotateAnimation = remember { Animatable(0f) }
+        val verticalMovement = remember { Animatable(100f) }
+        val horizontalMovement = remember { Animatable(100f) }
+        var resetPosition by remember { mutableStateOf(false) }
+
         LaunchedEffect(Unit) {
-            rotateAnimation.animateTo(720f, animationSpec = tween(durationMillis = 3000, easing = LinearEasing))
+            rotateAnimation.animateTo(
+                720f,
+                animationSpec = tween(durationMillis = 3000, easing = LinearEasing)
+            )
+            verticalMovement.animateTo(
+                600f,
+                animationSpec = tween(durationMillis = 3000, easing = LinearEasing)
+            )
+            horizontalMovement.animateTo(
+                600f,
+                animationSpec = tween(durationMillis = 3000, easing = LinearEasing)
+            )
+
         }
+//        LaunchedEffect (resetPosition){
+//            horizontalMovement.animateTo(100f, animationSpec = tween(durationMillis = 1000))
+//            verticalMovement.animateTo(100f, animationSpec = tween(durationMillis = 1000))
+//            resetPosition = false
+//        }
+//        // Button to reset the rectangle back to the center
+//        Button(
+//            onClick = {
+//                resetPosition = true
+//            },
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(16.dp)
+//        ) {
+//            Text("Reset to Original Spot")
+//        }
         Box(
             modifier = Modifier.graphicsLayer {
                 alpha = rotateAnimation.value
@@ -144,8 +174,8 @@ fun DragAndDropBoxes(modifier: Modifier = Modifier) {
 
         ) {
 //          drawCircle(Color.Green, radius = 50f, center = Offset(100f, 100f)
-            rotate(rotateAnimation.value,pivot = Offset(150f, 150f)) {
-                translate(left = 100f, top = 100f) {
+            rotate(rotateAnimation.value, pivot = Offset(150f, 150f)) {
+                translate(left = horizontalMovement.value, top = verticalMovement.value) {
                     drawRect(Color.Green, size = Size(100f, 100f))
                 }
             }
